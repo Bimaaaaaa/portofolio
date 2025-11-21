6f10d7b8cbddba85346155ee77173931de41b343
Bimaaaaaa <bmarzky.official@gmail.com>
2025-11-21 17:43:41 +0700

    update tampilan 2

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,17 +1,14 @@
-<head>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-</head>
+<div class="navbar">
+    <div class="profile">
+        <img src="assets/images/profile.jpg" alt="Bima's Profile">
+        <div class="name">Bima Rizki</div>
+        <div class="username">@bmarzky</div>
+    </div>
 
-<div class="profile">
-    <img src="assets/images/profile.jpg" alt="Bima's Profile">
-    <div class="name">Bima Rizki</div>
-    <div class="username">@bmarzky</div>
+    <ul>
+        <li><a href="./index.php"><i class="fas fa-home"></i> Home</a></li>
+        <li><a href="./about.php"><i class="fas fa-info-circle"></i> About</a></li>
+        <li><a href="./project.php"><i class="fas fa-project-diagram"></i> Projects</a></li>
+        <li><a href="./contact.php"><i class="fas fa-envelope"></i> Contact</a></li>
+    </ul>
 </div>
-
-<ul>
-    <li><a href="./index.php"><i class="fas fa-home"></i> Home</a></li> <!-- Ikon Rumah untuk Home -->
-    <li><a href="./about.php"><i class="fas fa-info-circle"></i> About</a></li> <!-- Ikon Info untuk About -->
-    <li><a href="./project.php"><i class="fas fa-project-diagram"></i> Projects</a></li> <!-- Ikon Proyek -->
-    <li><a href="./contact.php"><i class="fas fa-envelope"></i> Contact</a></li> <!-- Ikon Amplop untuk Contact -->
-</ul>
-
